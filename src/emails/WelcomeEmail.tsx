ba668e2e726b845b830c277e7b0d6ee1500379de
Kennedysenyo <kennedysenyo@gmail.com>
2025-06-18 23:36:47 +0000

    add contact email

--- src/emails/WelcomeEmail.tsx
+++ src/emails/WelcomeEmail.tsx
@@ -1,20 +1,62 @@
-import { Body, Head, Html, Preview, Text } from "@react-email/components";
+import {
+  Body,
+  Container,
+  Head,
+  Html,
+  Img,
+  Preview,
+  Text,
+} from "@react-email/components";
 
-export default function Welcome({ name }: { name: string }) {
+export default function Welcome({ name = "James Bond" }: { name: string }) {
   return (
     <Html>
       <Head />
       <Preview>Glad To Have You On Board.</Preview>
       <Body>
-        <Text>Hi, {name}</Text>
-        <Text>Welcome to StartAgri. We're excited to have you on board! </Text>
-        <Text>
-          Explore our products, book a consultation, or check out the lastest
-          farm updates.
-        </Text>
-        <Text>If you have any questions, we're just an email away.</Text>
-        <Text>Let's grow together!</Text>
-        <Text>-- The StartAgri Team</Text>
+        <Container>
+          <Container
+            style={{
+              display: "flex",
+              padding: "20px",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "green",
+            }}
+          >
+            <Img alt="logo" src="/img/logo.png" />
+            <Text
+              style={{
+                color: "white",
+                fontSize: "2rem",
+                textAlign: "center",
+              }}
+            >
+              StartAgri
+            </Text>
+          </Container>
+          <Container style={{ padding: "0 10px" }}>
+            <Text style={{ fontSize: "16px", color: "#111827" }}>
+              Hi, {name}
+            </Text>
+            <Text style={{ fontSize: "16px", color: "#111827" }}>
+              Welcome to StartAgri. We're excited to have you on board!{" "}
+            </Text>
+            <Text style={{ fontSize: "16px", color: "#111827" }}>
+              Explore our products, book a consultation, or check out the
+              lastest farm updates.
+            </Text>
+            <Text style={{ fontSize: "16px", color: "#111827" }}>
+              If you have any questions, we're just an email away.
+            </Text>
+            <Text style={{ fontSize: "16px", color: "#111827" }}>
+              Let's grow together!
+            </Text>
+            <Text style={{ fontSize: "16px", color: "#111827" }}>
+              -- The StartAgri Team
+            </Text>
+          </Container>
+        </Container>
       </Body>
     </Html>
   );
