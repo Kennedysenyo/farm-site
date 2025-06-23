ba59ea6136030d297c9fcb4bbc38adf9ec31faec
Kennedysenyo <kennedysenyo@gmail.com>
2025-06-23 15:36:22 +0000

    change flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,16 @@ import { ProcessSection } from "@/components/ProcessSection";
 import { ProductsSection } from "@/components/Products";
 import { ServicesSection } from "@/components/Services";
 import { TestimonialsSection } from "@/components/Testimonials";
+import { deleteCookie } from "@/hooks/handleCookies";
 import { useShowSocialLoginToast } from "@/hooks/useShowSocialLoginToast";
+import { showToast } from "@/utils/showToast";
 
 export default function Home() {
-  useShowSocialLoginToast();
+  const isSignIn = useShowSocialLoginToast();
+  if (isSignIn) {
+    showToast("success", "Signed In", "You have successfully signed in");
+  }
+  deleteCookie("signin-success");
   return (
     <div className="flex min-h-screen w-full flex-col">
       <Hero />
--- src/hooks/useShowSocialLoginToast.ts
+++ src/hooks/useShowSocialLoginToast.ts
@@ -1,21 +1,17 @@
 "use client";
 
-import { showToast } from "@/utils/showToast";
-import { useEffect } from "react";
-import { deleteCookie, hasCookie } from "./handleCookies";
+import { useEffect, useState } from "react";
+import { hasCookie } from "./handleCookies";
 
 export const useShowSocialLoginToast = () => {
+  const [cookieExist, setCookieExist] = useState(false);
   useEffect(() => {
     console.log("useSocialLoginToast is running...");
     const showSignInToast = async () => {
-      const signedIn = hasCookie("signin-success");
-
-      if (signedIn) {
-        showToast("success", "Signed In", "You have successfully signed in");
-      }
-      deleteCookie("signin-success");
+      setCookieExist(hasCookie("signin-success"));
     };
 
     showSignInToast();
   }, []);
+  return cookieExist;
 };
