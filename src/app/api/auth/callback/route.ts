d844cf1062197330690d5cc544baafc97a693926
Kennedysenyo <kennedysenyo@gmail.com>
2025-06-23 10:58:19 +0000

    select user from array

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -32,7 +32,7 @@ export async function GET(request: Request) {
           .from(users)
           .where(eq(users.id, id));
 
-        if (!userExist) {
+        if (!userExist[0]) {
           await db.insert(users).values({
             id,
             firstName: user.user_metadata.firstName,
