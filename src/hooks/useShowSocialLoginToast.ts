3cba74655b63bb7ec9e27dca45fbbe8ab2a31d61
Kennedysenyo <kennedysenyo@gmail.com>
2025-06-23 16:00:15 +0000

    remove unused useState import

--- src/hooks/useShowSocialLoginToast.ts
+++ src/hooks/useShowSocialLoginToast.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { deleteCookie, hasCookie } from "./handleCookies";
 import { showToast } from "@/utils/showToast";
 
